00432e88ac35cbf222bc1bf95356c8337ed3fa88
davinrdh <davinridhas@gmail.com>
2023-06-16 14:03:15 +0700

    sangadol

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  @ViewChild('scrollTrigger') scrollTrigger!: ElementRef;
+
+  ngOnInit(): void {
+    this.scrollTrigger.nativeElement.addEventListener('click', () => {
+      this.hideNavbarCollapse();
+    });
+  }
+
+  hideNavbarCollapse(): void {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse) {
+      navbarCollapse.classList.remove('show');
+    }
+  }
 }
